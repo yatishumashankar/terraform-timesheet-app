3637cf586981f7c6371d8b939116c34451cc4969
yatishumashankar <yatishumashankar@GMAIL.COM>
2026-03-31 00:00:30 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,61 +1,88 @@
+# -------------------------------
 # VPC
+# -------------------------------
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+  cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "timesheet-vpc"
   }
 }
 
-# Public Subnet
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnet_cidr
-  availability_zone       = var.availability_zone
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet"
-  }
-}
-
-# Private Subnet
-resource "aws_subnet" "private" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnet_cidr
-  availability_zone = var.availability_zone
-
-  tags = {
-    Name = "private-subnet"
-  }
-}
-
-# Internet Gateway
+# -------------------------------
+# INTERNET GATEWAY
+# -------------------------------
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "main-igw"
+    Name = "timesheet-igw"
   }
 }
 
-# Route Table
+# -------------------------------
+# PUBLIC SUBNET
+# -------------------------------
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "timesheet-public-subnet"
+  }
+}
+
+# -------------------------------
+# PRIVATE SUBNET 1
+# -------------------------------
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "timesheet-private-subnet-1"
+  }
+}
+
+# -------------------------------
+# PRIVATE SUBNET 2
+# -------------------------------
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "timesheet-private-subnet-2"
+  }
+}
+
+# -------------------------------
+# PUBLIC ROUTE TABLE
+# -------------------------------
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "public-route-table"
+    Name = "timesheet-public-rt"
   }
 }
 
-# Route to Internet
+# -------------------------------
+# ROUTE TO INTERNET
+# -------------------------------
 resource "aws_route" "internet_access" {
   route_table_id         = aws_route_table.public_rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
 
-# Associate Public Subnet
+# -------------------------------
+# ASSOCIATE PUBLIC SUBNET
+# -------------------------------
 resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public_rt.id
