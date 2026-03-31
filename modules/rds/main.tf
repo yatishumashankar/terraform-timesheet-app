05411f2576130493c67dd14d3ce2173bf7a6de1a
yatishumashankar <yatishumashankar@GMAIL.COM>
2026-03-31 11:25:16 +0530

    Update main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,6 +1,6 @@
 resource "aws_db_subnet_group" "db_subnet" {
   name       = "timesheet-db-subnet-group"
-  subnet_ids = [var.private_subnet_id]
+  subnet_ids = var.private_subnet_ids
 
   tags = {
     Name = "timesheet-db-subnet-group"
@@ -9,13 +9,13 @@ resource "aws_db_subnet_group" "db_subnet" {
 
 resource "aws_db_instance" "db" {
   allocated_storage    = 20
-  engine               = "mysql"
-  instance_class       = "db.t3.micro"
-  db_name              = "timesheetdb"
-  username             = "admin"
-  password             = "Admin1234"
-  skip_final_snapshot  = true
+  engine              = "mysql"
+  instance_class      = "db.t3.micro"
+  db_name             = "timesheetdb"
+  username            = "admin"
+  password            = "Admin1234"
+  skip_final_snapshot = true
 
-  db_subnet_group_name = aws_db_subnet_group.db_subnet.name
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet.name
   vpc_security_group_ids = [var.security_group_id]
 }
